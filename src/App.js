94f68b4c721d85c334e452ef04652b69aea18e87
knockknockhusthere <angela.huxia@gmail.com>
2018-05-29 15:40:27 -0700

    created ChatLog components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 class App extends Component {
   render() {
     console.log(chatMessages);
 
+    const chatLog = <ChatLog messages={chatMessages} />;
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Chatlog</h1>
         </header>
         <main className="App-main">
+          <section>
+            {chatLog}
+          </section>
         </main>
       </div>
     );
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+  render() {
+    // console.log(chatMessages);
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Message;
